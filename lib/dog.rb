cd04c3c4b6a7ea4340faba64fb570b8b09c8a671
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-29 19:09:01 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def init_name(name)
+    @name = name 
+  end 
+  def name 
+    @name 
+  end
+  def breed_def(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end 
